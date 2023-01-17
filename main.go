d04bd37341f850fed98dbef6ff2c535929143b96
Герман Чердакли <g.cherdakly@int-sys.ru>
2023-01-17 19:32:06 +0300

    enum for web server states

--- main.go
+++ main.go
@@ -33,14 +33,14 @@ func initSessionStorage(cookie_name string, timeout time.Duration) *SessionStora
 }
 
 // concurrently get map entry, returns error on non-existent key
-func (ses *SessionStorage) get(key string) (*Session, error) {
+func (session_storage *SessionStorage) get(key string) (*Session, error) {
 	var session *Session
 	var exists bool
 
-	ses.mux.Lock()
-	defer ses.mux.Unlock()
+	session_storage.mux.Lock()
+	defer session_storage.mux.Unlock()
 
-	session, exists = ses.hashmap[key]
+	session, exists = session_storage.hashmap[key]
 	if !exists {
 		return nil, fmt.Errorf("Key error: session %s does not exist", key)
 	}
@@ -49,40 +49,70 @@ func (ses *SessionStorage) get(key string) (*Session, error) {
 }
 
 // concurrently set map entry
-func (ses *SessionStorage) set(key string, value *Session) {
-	ses.mux.Lock()
-	defer ses.mux.Unlock()
+func (session_storage *SessionStorage) set(key string, value *Session) {
+	session_storage.mux.Lock()
+	defer session_storage.mux.Unlock()
 
-	ses.hashmap[key] = value
+	session_storage.hashmap[key] = value
 }
 
 // concurrently delete map entry
-func (ses *SessionStorage) delete(key string) {
-	ses.mux.Lock()
-	defer ses.mux.Unlock()
+func (session_storage *SessionStorage) delete(key string) {
+	session_storage.mux.Lock()
+	defer session_storage.mux.Unlock()
 
-	delete(ses.hashmap, key)
+	delete(session_storage.hashmap, key)
 }
 
 // deletes opened sessions with expiration time before <time>
 // returns slice of usernames from closed sessions
-func (ses *SessionStorage) cleanupOlder(time time.Time) []string {
+// TODO: run as a coroutine
+func (session_storage *SessionStorage) cleanupOlder(time time.Time) []string {
 	var result []string = []string{}
-	ses.mux.Lock()
-	defer ses.mux.Unlock()
+	session_storage.mux.Lock()
+	defer session_storage.mux.Unlock()
 
-	for key, value := range ses.hashmap {
+	for key, value := range session_storage.hashmap {
 		if value.expirationTime.Before(time) {
 			result = append(result, value.username)
-			delete(ses.hashmap, key)
+			delete(session_storage.hashmap, key)
 		}
 	}
 	return result
 }
 
+type pageUrl int
+
+const (
+	MAIN pageUrl = iota
+	LOGIN
+	PROJECT
+	DOWNLOADS
+	FILE
+	LOGOUT
+)
+
+func (page pageUrl) String() string {
+	switch page {
+	case MAIN:
+		return "/"
+	case LOGIN:
+		return "/login"
+	case PROJECT:
+		return "/project/{}"
+	case DOWNLOADS:
+		return "/downloads"
+	case FILE:
+		return "/file/{name}"
+	case LOGOUT:
+		return "/logout"
+	}
+	return "/"
+}
+
 func authMiddleware(next http.HandlerFunc, session_storage *SessionStorage) http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
-		var redirect_handler http.Handler = http.RedirectHandler("/login", http.StatusSeeOther)
+		var redirect_handler http.Handler = http.RedirectHandler(LOGIN.String(), http.StatusFound)
 		var cookie *http.Cookie
 		var session *Session
 		var err error
@@ -116,7 +146,7 @@ func pageLogin(bot *LdapBot, session_storage *SessionStorage) http.HandlerFunc {
 
 		// on GET
 		if request.Method != http.MethodPost {
-			tmpl, err = template.ParseFiles("login.html")
+			tmpl, err = template.ParseFiles("templates/login.html")
 			if err != nil {
 				http.Error(writer, "not found", http.StatusNotFound)
 				log.Fatal("No template file found")
@@ -130,7 +160,7 @@ func pageLogin(bot *LdapBot, session_storage *SessionStorage) http.HandlerFunc {
 		err = request.ParseForm()
 		if err != nil {
 			log.Println("Form could not be processed")
-			http.Redirect(writer, request, "/login", http.StatusSeeOther)
+			http.Redirect(writer, request, LOGIN.String(), http.StatusFound)
 			return
 		}
 
@@ -138,7 +168,7 @@ func pageLogin(bot *LdapBot, session_storage *SessionStorage) http.HandlerFunc {
 		uuid, err = (*bot).authorizeUser(request.Form["username"][0], request.Form["password"][0])
 		if err != nil {
 			log.Printf("Error while authorizing user: %s\n", err)
-			http.Redirect(writer, request, "/login", http.StatusSeeOther)
+			http.Redirect(writer, request, LOGIN.String(), http.StatusSeeOther)
 			return
 		}
 
@@ -149,7 +179,7 @@ func pageLogin(bot *LdapBot, session_storage *SessionStorage) http.HandlerFunc {
 			expirationTime: time.Now().Add(session_storage.defaultSessionTimeout),
 		})
 
-		http.Redirect(writer, request, "/", http.StatusFound)
+		http.Redirect(writer, request, MAIN.String(), http.StatusFound)
 	}
 }
 
@@ -174,6 +204,31 @@ func pageDownload() http.HandlerFunc {
 	}
 }
 
+func logout(session_storage *SessionStorage) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		if request.Method != http.MethodDelete {
+			http.Error(writer, "Accepts delete requests only", http.StatusBadRequest)
+		}
+
+		var cookie *http.Cookie
+		var err error
+
+		cookie, err = request.Cookie(session_storage.defaultCookieName)
+		if err != nil {
+			http.Error(writer, "not logged in", http.StatusForbidden)
+			return
+		}
+
+		_, err = session_storage.get(cookie.Value)
+		if err != nil {
+			http.Error(writer, "not logged in", http.StatusForbidden)
+			return
+		}
+		session_storage.delete(cookie.Value)
+		http.Redirect(writer, request, MAIN.String(), http.StatusFound)
+	}
+}
+
 func main() {
 	var err error
 	var config *Config
@@ -192,13 +247,14 @@ func main() {
 		config.Settings.Ldap.Password, config.Settings.Ldap.SearchBaseDn,
 		config.Settings.Ldap.SearchBaseFilter)
 
-	http.HandleFunc("/login", pageLogin(bot, session_storage))
-	http.HandleFunc("/", authMiddleware(pageMain(), session_storage))
-	http.HandleFunc("/download", authMiddleware(pageMain(), session_storage))
-	http.HandleFunc("/project/{id}", authMiddleware(pageProject(), session_storage))
+	http.HandleFunc(LOGIN.String(), pageLogin(bot, session_storage))
+	http.HandleFunc(MAIN.String(), authMiddleware(pageMain(), session_storage))
+	http.HandleFunc(DOWNLOADS.String(), authMiddleware(pageMain(), session_storage))
+	http.HandleFunc(PROJECT.String(), authMiddleware(pageProject(), session_storage))
 
+	log.Printf("Starting web server on port %d", config.Settings.Web.Port)
 	err = http.ListenAndServe(fmt.Sprintf(":%d", config.Settings.Web.Port), nil)
 	if err != nil {
-		log.Fatal("Http server fatal error: ", err)
+		log.Panicf("Http server stopped with fatal error: %s", err)
 	}
 }
